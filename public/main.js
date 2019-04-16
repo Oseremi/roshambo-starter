7befefbf62b7b879ac92435e079c28c05ff9afd9
Oseremi <oseremi@gmail.com>
2019-04-15 20:22:49 -0400

    In Class Demo-Roshambo

--- public/main.js
+++ public/main.js
@@ -1,45 +1,76 @@
-const $ = s => document.querySelector(s)
-const $$ = s => document.querySelectorAll(s)
+const $ = s => document.querySelector(s);
+const $$ = s => document.querySelectorAll(s);
 
-const handleButtonClick = (event) => {
-  console.log('clicked')
-  const player = event.target.className
-  const computer = getComputerMove()
-  $('figure.player img').src = `https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/${player}.svg`
-  $('figure.computer img').src = `https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/${computer}.svg`
+const handleButtonClick = event => {
+  const player = event.target.className;
+  const computer = getComputerMove();
+  console.log(player, computer);
+  $(
+    "figure.player img"
+  ).src = `https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/${player}.svg`;
+  $(
+    "figure.computer img"
+  ).src = `https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/${computer}.svg`;
+
+  //rock beats scissors
+  if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "scissors" && computer === "paper") ||
+    (player === "paper" && computer === "rock")
+  ) {
+    console.log("player wins");
+  }
+  if (
+    (computer === "rock" && player === "scissors") ||
+    (computer === "scissors" && player === "paper") ||
+    (computer === "paper" && player === "rock")
+  ) {
+    console.log("player loses");
+  }
+
+  // scissors beats paper
+
+  // paper beats rock
+
+  // if player === computer it's a draw
+  if (player === computer) {
+    console.log("draw");
+  }
 
   // HINT: Check for win, lose or draw, then call `gameOver()` eventually.
-}
+};
 
 const getComputerMove = () => {
-  const moves = ['rock', 'paper', 'scissors']
-  return moves[Math.floor(Math.random() * moves.length)]
-}
+  const moves = ["rock", "paper", "scissors"];
+  return moves[Math.floor(Math.random() * moves.length)];
+};
 
 // HINT: Try calling `gameOver(true)` in the console.
-const gameOver = (playerDidWin) => {
+const gameOver = playerDidWin => {
   if (playerDidWin) {
-    $('.dialog h3').textContent = 'You won!'
+    $(".dialog h3").textContent = "You won!";
   } else {
-    $('.dialog h3').textContent = 'You lost!'
+    $(".dialog h3").textContent = "You lost!";
   }
-  $('body').className = 'modal'
-}
+  $("body").className = "modal";
+};
 
 const resetGame = () => {
   // TODO: Probably need to do more to reset the game here...
-  $('figure.player img').src = 'https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/unknown.svg'
-  $('figure.computer img').src = 'https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/unknown.svg'
-  $('body').className = ''
-}
+  $("figure.player img").src =
+    "https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/unknown.svg";
+  $("figure.computer img").src =
+    "https://tiy-tpa-fee.github.io/roshambo/starter-kit/images/unknown.svg";
+  $("body").className = "";
+};
 
 const main = () => {
-  console.log('started')
-  const buttons = $$('.player-input button')
+  console.log("started");
+  const buttons = $$(".player-input button");
   for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', handleButtonClick)
+    buttons[i].addEventListener("click", handleButtonClick);
   }
-  $('.dialog button').addEventListener('click', resetGame)
-}
+  $(".dialog button").addEventListener("click", resetGame);
+};
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener("DOMContentLoaded", main);
